b545ac1f519155cefb95a9186b6fc85d0aa9dddf
George Kramer <george.kramer@live.com>
2021-04-06 19:50:45 -0700

    Use verbs as subcommands for each feature
    Want to avoid using individual projects because the amount of code
    sharing will be high, and the features that will be implemented are all
    related.

--- src/Program.cs
+++ src/Program.cs
@@ -9,17 +9,51 @@ using System;
 namespace ColorModel {
     public class Program {
         public static void Main(string[] args) {
-            Parser.Default.ParseArguments<Options>(args)
-            .WithParsed<Options>(RunOptions);
-            //.WithNotParsed<Options>(HandleParseError);
+            Parser.Default.ParseArguments<DownloadOptions, FilterOptions>(args)
+            .WithParsed<DownloadOptions>(RunDownloadOptions)
+            .WithParsed<FilterOptions>(RunFilterOptions);
         }
 
-        public class Options {
-            [Option("db", Required = false, Default = "colors.db", HelpText = "Path to a SQLite database file")]
+        [Verb("filter", isDefault: false, HelpText = "Filter color data")]
+        public class FilterOptions {
+            [Option("db", Required = true, HelpText = "Path to a SQLite database file containing downloaded color data")]
             public string DbFile { get; set; }
+
+            [Option("min-hue", Required = false, Default = 0.0f, HelpText = "Min hue value")]
+            public double MinHue { get; set; }
+            [Option("max-hue", Required = false, Default = 360.0f, HelpText = "Max hue value")]
+            public double MaxHue { get; set; }
+
+            [Option("min-saturation", Required = false, Default = 0.0f, HelpText = "Min saturation value")]
+            public double MinSaturation { get; set; }
+            [Option("max-saturation", Required = false, Default = 1.0f, HelpText = "Max saturation value")]
+            public double MaxSaturation { get; set; }
+
+            [Option("min-lightness", Required = false, Default = 0.0f, HelpText = "Min lightness value")]
+            public double MinLightness { get; set; }
+            [Option("max-lightness", Required = false, Default = 1.0f, HelpText = "Max lightness value")]
+            public double MaxLightness { get; set; }
+
+            [Option("min-lrv", Required = false, Default = 0.0, HelpText = "Min LRV value")]
+            public double MinLrv { get; set; }
+            [Option("max-lrv", Required = false, Default = 100.0, HelpText = "Max LRV value")]
+            public double MaxLRV { get; set; }
         }
 
-        private static void RunOptions(Options opts) {
+        [Verb("download", isDefault: false, HelpText = "Download color data")]
+        public class DownloadOptions {
+            [Option("db", Required = false, Default = "colors.db", HelpText = "Path to a SQLite database file to which color data is downloaded")]
+            public string DbFile { get; set; }
+
+            [Option("benjamin-moore", Required = false, Default = false, HelpText = "Whether to download Benjamin Moore color data")]
+            public bool HasBenjaminMoore { get; set; }
+        }
+
+        private static void RunFilterOptions(FilterOptions opts) {
+
+        }
+
+        private static void RunDownloadOptions(DownloadOptions opts) {
             using ColorContext colorContext = new ColorContext(opts.DbFile);
             colorContext.Database.EnsureCreated();
 
