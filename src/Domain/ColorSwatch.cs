32e947841d54fb87bb4daac78f81edd71a7c8bd7
George Kramer <george.kramer@live.com>
2021-04-10 19:08:50 -0700

    Print a color swatch more nicely
    Rather than zero-pad the hue, space-pad it to make it more legible.
    
    Make saturation and lightness go out to four decimal digits, since
    multiplying by 100 creates a percentage and want two decimal digits
    afterwards in that case.  This makes all numeric values effectively have
    two decimal digits.

--- src/Domain/ColorSwatch.cs
+++ src/Domain/ColorSwatch.cs
@@ -17,7 +17,7 @@ namespace Painter.Domain {
         public double Lrv { get; set; }
 
         public override string ToString() {
-            return Hue.ToString("000.000") + ", " + Saturation.ToString("0.000") + ", " + Lightness.ToString("0.000") + ", " + Lrv.ToString("0.00") + ": " + Name + " (" + Brand.GetDescription() + " " + string.Join(", ", ColorNumbers.Select(x => x.Number)) + ")";
+            return Hue.ToString("0.00").PadLeft(6) + ", " + Saturation.ToString("0.0000") + ", " + Lightness.ToString("0.0000") + ", " + Lrv.ToString("0.00") + ": " + Name + " (" + Brand.GetDescription() + " " + string.Join(", ", ColorNumbers.Select(x => x.Number)) + ")";
         }
     }
 }
